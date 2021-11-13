003b56182f6e124235ac7bc3f304a92375e7f507
Tobias Goddard <tobyg7794@hotmail.com>
2021-11-13 18:00:32 +0000

    Git ignore added and log4j configuration complete

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+<
+logs/log.log,5\5\556ba4b2b1dccf4c3c379694e46cd98762049bab
--- src/main/java/tobyg/App.java
+++ src/main/java/tobyg/App.java
@@ -1,5 +1,8 @@
 package tobyg;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Logger lg = LogManager.getLogger(App.class);
+        lg.info("test");
+
     }
 }
